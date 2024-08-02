0ce4a205225c96f670052a126555a009cb771524
Hamit Mızrak <hamitmizrak@gmail.com>
2024-08-02 14:48:12 +0300

    Main, ClassComponent, FunctionComponent

---
+++ src/component/counter/CounterClass.jsx
@@ -0,0 +1,20 @@
+// rcc = TAB (React JS)
+
+// REACT LIBRARIES
+import React, {Component} from 'react';
+
+
+// REACT NATIVE LIBRARIES
+import {View, Text} from 'react-native';
+
+
+// CLASS COUNTER
+export default class CounterClass extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Counter Class</Text>
+      </View>
+    ); //end return
+  } //end render
+} //end class
---
+++ src/component/counter/CounterFunction.jsx
@@ -0,0 +1,19 @@
+// rfce => TAB (React JS)
+
+// REACT LIBRARIES
+import React from 'react';
+
+// REACT NATIVE LIBRARIES
+import {View, Text} from 'react-native';
+
+// FUNCTION COUNTER
+function CounterFunction() {
+  return (
+    <View>
+      <Text>Counter Function</Text>
+    </View>
+  ); //end return
+} //end Function
+
+// EXPORT
+export default CounterFunction;
