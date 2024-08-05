6c5bc3912d6adaa2c04ae88d5eed3f490745b26e
Hamit Mızrak <hamitmizrak@gmail.com>
2024-08-05 21:42:09 +0300

    Counter Hooks

--- src/component/counter/CounterFunctionHooks.jsx
+++ src/component/counter/CounterFunctionHooks.jsx
@@ -11,10 +11,25 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // REACT
-import React, {useState,useEffect,useRef,useMemo,useCallback,useReducer} from 'react'
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useMemo,
+  useCallback,
+  useReducer,
+} from 'react';
 
 // REACT NATIVE
-import { StyleSheet, Text, View,Modal,ImageBackground,TouchableOpacity } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 
 // Vector Icons
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -40,28 +55,212 @@ const AlertCard = ({visible, onClose, message}) => {
   ); //end return
 }; //end AlertCard
 
-
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 
+// useReducer : Birden fazla durumun birlikte çalışması gerektiği zamanlarda kullanılabilir.
+// useReducer : Karmaşık durum(state) yönetimi zor olan yapılarda kullanılabilir.
+// useReducer : Birden gazla ilgili durumların birlikte çalışması gerektiği zamanlarda kullanırız.
+// Durum güncellemerini daha kontrollü ve öngürebilir olmasını sağlar.
+// reducer(state, action)
+// state: Mevcut durumu temsil
+// action: Durumuhn nasıl değişceğini tanımlar
+// reducer: initialState başlangıç durumudur.
+// dispatch: Durumunu güncellemek için kullanılan fonksiyondur.
 
+const initialState = {count: 0};
 
+// reducer fonksiyonları
+// Switch ifadesinde hangi eylem türlerinde nasıl bir durum olduğunda ne yapacağını tanımlar.
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    case 'reset':
+      return initialState;
+    default:
+      throw new Error();
+  } //end switch
+} //end reducer
 
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 
 // FUNCTION
 const CounterFunctionHooks = () => {
+  ///////////////////////////
+  // STATE
+  const [isAlertVisible, setIsAlertVisible] = useState(false); // Modal
+  const [state, dispatch] = useReducer(reducer, initialState); // useReducer
+  const buttonClickCount = useRef(0); //
+  const minusCount = 5; // sayacın azaltacağı değeri
+
+  ///////////////////////////
+  // EFFECTS
+  // Bileşenlerde yükleme veya güncelleme yapıldığında çalışır.
+  // Örneğin: fetching, DOM güncelleştirmelerinde
+  // 2 parametre olarak alır.
+  // 1.parametre Yan etki fonksiyonu(Effect Function)
+  // 2.parametre Bağımlılıklar dizi: Eğer boş bir dizi bırakılırsa yan etki fonksiyonu yalnızca ilk kez yüklendiğinde çalışır (componentDidMount)
+  // Eğer bağımlılıklar diziye bir eleman eklenirse, y bu değer değiştiğinde yan etki fonksiyonu yani 1.parametrede çalışır.
+  useEffect(() => {
+    // Asyn-Await
+    // 1.YOL
+    /*
+    const checkCounterAsyncAwait = async () => {
+      console.log("Counter Function useEffect Hook: counter 5 oldu!");
+      await new Promise(resolve => {
+        if (state.count === minusCount) {
+          Alert.alert('Sayaç 5 oldu!');
+          // 2 saniye sonra göstersin
+          const timeoutModalId= setTimeout(() => {
+             //showAlert();
+            setIsAlertVisible(true);
+          }, 2000);
+    
+          // Temizleme işlemlerinde
+          return() => {
+            clearTimeout(timeoutModalId);
+          }
+        }
+      })
+    };//end asyn
+    // Async-Await Calling
+    checkCounterAsyncAwait();
+    */
+
+    // 2.YOL
+    if (state.count === minusCount) {
+      setIsAlertVisible(true); // Alert aç
+
+      /*
+      buttonClickCount.current++; // Alertın kaç kez tıklandığını tutar
+      setTimeout(() => {
+        setIsAlertVisible(false); // Alert kapanır
+        dispatch({type:'reset'}); // sayacı sıfırlar
+      }, 2000); // Alertın kaç saniye süreceklerini tutar
+      */
+    } //end if
+
+    // useEffect
+    console.log(`Counter Function useEffect Hook: ${state.count}`);
+
+    // useRef kullanımı
+    console.log(`Button Clicked Count: ${buttonClickCount.current} times`);
+
+    // Temizleme işlem örneği
+    return () => {
+      console.log('useEffect cleanup');
+      console.log('Counter Function useEffect Hook: Component will unmount');
+    }; //end useEffect
+  }, [state.count]); //end useEffect
+
+  ///////////////////////////
+  // useMemo: counter değerinin 2 katı, 2. parametre olarak verilerek optimize edilir.
+  /*
+  useMemo: optimizasyonu sağlar.  hesaplamalar sonucu hatırlamak için değişkenlerde 1 tanesi değiştinde değişirir.
+  */
+  const doubledValue = useMemo(() => {
+    console.log('Counter Function useMemo Hook: Calculating doubledValue');
+    return state.count * 2;
+  });
+
+  ///////////////////////////
+  // useCallback: Artırma ve azaltma fonksiyonlar
+  // useCallback : bir fonksiyonun referansını yeniden oluşturmayı engeller
+  // useCallback: optimizasyonu sağlar.
+
+  // Artırma
+  const handleIncrease = useCallback(() => {
+    dispatch({type: 'increment'});
+    buttonClickCount.current++;
+    //buttonClickCount.current+=1;
+  }, []);
+
+  // Azaltma
+  const handleDecrease = useCallback(() => {
+    if (buttonClickCount.current <= 0) {
+      Alert.alert("Counter Function\nSayaç 0'dan küçük olamaz!");
+    } else {
+      dispatch({type: 'decrement'});
+      buttonClickCount.current--;
+      //buttonClickCount.current-=1;
+    }
+  }, []);
+
+  // Sıfırlama(Resetleme)
+  const handleReset = () => {
+    dispatch({type: 'reset'});
+    buttonClickCount.current = 0;
+  };
+
+  ///////////////////////////
+  // Modal Kapatma fonksiyonları
+  const closeAlert = () => {
+    setIsAlertVisible(false);
+  };
+
+  // showAlert Alert butona tıkladığımda göstersin
+  const showAlert = () => {
+    setIsAlertVisible(true);
+  };
+
+  ///////////////////////////
+  // Return
   return (
-    <View>
-      <Text>CounterFunctionHooks</Text>
-    </View>
-  )
-}
+    <ImageBackground
+      //source={{uri: 'https://via.placeholder.com/600x800'}} //internet uri
+      source={require('../../assets/device.webp')}
+      style={styles.imageBackground}>
+      <View style={styles.container}>
+        <Text style={styles.counterText}> Sayaç: {state.count}</Text>
+        <Text style={styles.counterText}> Sayaçın 2 Katı: {doubledValue}</Text>
+        <View style={styles.buttonContainer}>
+          {/* 1.YOL ARTIRMA */}
+          <TouchableOpacity
+            style={[styles.button, {backgroundColor: 'blue'}]}
+            onPress={handleIncrease}>
+            <Icon name="plus" size={18} color="#fff" />
+            <Text style={styles.buttonText}>Artır</Text>
+          </TouchableOpacity>
+          {/* 2.YOL ARTIRMA */}
+          {/* <Button title="Sayaç Artırma" onPress={handleIncrease} /> */}
+
+          {/* 1.YOL AZALTMA */}
+          <TouchableOpacity
+            style={[styles.button, {backgroundColor: 'green'}]}
+            onPress={handleDecrease}>
+            <Icon name="minus" size={18} color="#fff" />
+            <Text style={styles.buttonText}>Azalt</Text>
+          </TouchableOpacity>
+          {/* 2.YOL AZALTMA */}
+          {/* <Button title="Sayaç Azalt" onPress={this.handleDecrease} /> */}
+
+          {/* TEMİZLEME(RESET) */}
+          <TouchableOpacity
+            style={[styles.button, {backgroundColor: 'red'}]}
+            onPress={handleReset}>
+            <Icon name="refresh" size={18} color="#fff" />
+            <Text style={styles.buttonText}>Temizle</Text>
+          </TouchableOpacity>
+
+          {/* ALERT MODAL */}
+          <AlertCard
+            visible={isAlertVisible}
+            onClose={closeAlert}
+            // message={`Sayaç değeri: ${buttonClickCount.current}`}
+            message={"Sayaç değeri: "+ buttonClickCount.current}
+          />
+        </View>
+      </View>
+    </ImageBackground>
+  ); //end return
+}; //end CounterFunctionHooks
 
 // EXPORT
-export default CounterFunctionHooks
-
+export default CounterFunctionHooks;
 
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -116,6 +315,7 @@ const styles = StyleSheet.create({
     // borderRadius:10,
   },
 
+  // Alert Box
   alertBox: {
     width: 300,
     padding: 20,
@@ -125,11 +325,15 @@ const styles = StyleSheet.create({
     // shadowColor: '#000',
     // shadowOffset: {width: 0, height: 2},
   },
+
+  // Alert Message
   alertMessageer: {
     fontSize: 18,
     marginBottom: 20,
     textAlign: 'center',
   },
+
+  // Alert Close Button
   alertCloseButton: {
     top: 10,
     right: 10,
@@ -150,9 +354,10 @@ const styles = StyleSheet.create({
     // shadowOffset: {width: 0, height: 2},
     // alignItems:'center',
   },
+
+  // Alert Close ButtonText
   alertCloseButtonText: {
     color: '#ffffff',
     fontSize: 16,
   },
 }); //end styles
-
